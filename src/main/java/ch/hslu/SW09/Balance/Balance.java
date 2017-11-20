3ad61c39c99afc81ab8cb0b7043fc384cd1ac5f0
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2017-11-20 16:31:19 +0100

    sw09 cyber dojo

---
+++ src/main/java/ch/hslu/SW09/Balance/Balance.java
@@ -0,0 +1,48 @@
+package ch.hslu.SW09.Balance;
+
+import java.util.Stack;
+
+public class Balance {
+
+    public boolean isBalanced(String testString) {
+        Stack<String> stack = new Stack<>();
+        for ( int i = 0; i < testString.length(); i++ ){
+            char currentCharakter = testString.charAt(i);
+
+            if (isOpeningChar(String.valueOf(currentCharakter))){
+                stack.push(String.valueOf(currentCharakter));
+            } else if(isClosingChar(String.valueOf(currentCharakter))){
+                if(!isCorrespondingClosingChar(stack.pop(), String.valueOf(currentCharakter))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public boolean isOpeningChar (String charakter) {
+        if (charakter.equals("(") || charakter.equals("{") || charakter.equals("[")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isClosingChar (String charakter) {
+        if (charakter.equals(")") || charakter.equals("}") || charakter.equals("]")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isCorrespondingClosingChar (String lastOpeningChar, String closingChar) {
+        if (lastOpeningChar.equals("(") && closingChar.equals(")")){
+            return true;
+        } else if (lastOpeningChar.equals("{") && closingChar.equals("}")){
+            return true;
+        } else if (lastOpeningChar.equals("[") && closingChar.equals("]")){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/ch/hslu/SW09/Temperatur/Balance/BalanceTest.java
@@ -0,0 +1,65 @@
+package ch.hslu.SW09.Temperatur.Balance;
+
+import ch.hslu.SW09.Balance.Balance;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class BalanceTest {
+
+    @Test
+    public void testunbalancedParentheses() {
+        String parentheses = "{{)(}}";
+        Balance balance = new Balance();
+
+        boolean expected = false;
+        boolean actual = balance.isBalanced(parentheses);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testunbalancedParentheses2() {
+        String parentheses = "({)}";
+        Balance balance = new Balance();
+
+        boolean expected = false;
+        boolean actual = balance.isBalanced(parentheses);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParentheses() {
+        String parentheses = "[({})]";
+        Balance balance = new Balance();
+
+        boolean expected = true;
+        boolean actual = balance.isBalanced(parentheses);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParentheses1() {
+        String parentheses = "{}([])";
+        Balance balance = new Balance();
+
+        boolean expected = true;
+        boolean actual = balance.isBalanced(parentheses);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParentheses2() {
+        String parentheses = "{()}[[{}]]";
+        Balance balance = new Balance();
+
+        boolean expected = true;
+        boolean actual = balance.isBalanced(parentheses);
+
+        assertEquals(expected, actual);
+    }
+
+
+}
